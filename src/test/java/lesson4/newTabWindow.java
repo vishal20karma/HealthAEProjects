0c573282d71da30185aa8c1c9907a3bfe07ddbf2
vishal <vishal.vishwakarma20@gmail.com>
2025-06-20 00:14:59 +0530

    new changes

--- src/test/java/lesson4/newTabWindow.java
+++ src/test/java/lesson4/newTabWindow.java
@@ -5,6 +5,7 @@ import java.util.Iterator;
 import java.util.Set;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
